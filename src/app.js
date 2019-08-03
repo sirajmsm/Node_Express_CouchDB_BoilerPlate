86d7e0def8a30fedc7de0f7bffc6b5244b71e1da
sirajmsm <msmsiraj4u@gmail.com>
2019-08-03 20:15:02 +0530

    added chalk, nodemon dependencies

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log("calling app js");
+const express = require('express');
+const chalk = require('chalk');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>console.log(`%s App is running at http://localhost: ${PORT} `, chalk.green('✓')));
