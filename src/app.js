d024c6c49b2e35a614c4c0c52818f88afc6cbdc9
sirajmsm <msmsiraj4u@gmail.com>
2019-08-03 20:28:09 +0530

    added sample service

--- src/app.js
+++ src/app.js
@@ -3,4 +3,8 @@ const chalk = require('chalk');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use('/users',(req,res,next)=>{
+    res.send("initail start up");
+});
+
 app.listen(PORT,()=>console.log(`%s App is running at http://localhost: ${PORT} `, chalk.green('✓')));
